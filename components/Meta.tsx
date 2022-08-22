124516175e093e28557f32a99d37ce1c5366260b
Sam Poate <sampoate@outlook.com>
2022-08-22 22:54:31 +0100

    Update meta and favicon

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -6,17 +6,9 @@ interface MetaProps {}
 export const Meta: React.FC<MetaProps> = () => {
   return (
     <Head>
-      {/* <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
-      <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
-      <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
-      <link rel='manifest' href='/favicon/site.webmanifest' />
-      <link rel='mask-icon' href='/favicon/safari-pinned-tab.svg' color='#000000' />
-      <link rel='shortcut icon' href='/favicon/favicon.ico' /> */}
       <meta name='msapplication-TileColor' content='#000000' />
-      {/* <meta name='msapplication-config' content='/favicon/browserconfig.xml' /> */}
-      <meta name='theme-color' content='#000' />
-      <link rel='alternate' type='application/rss+xml' href='/feed.xml' />
-      <meta name='description' content={`A statically generated blog example using Next.js and Strapi.`} />
+      <meta name='theme-color' content='#191919' />
+      <meta name='description' content='Home of the Athlete Movement Classes' />
       <meta property='og:image' content={HOME_OG_IMAGE_URL} />
     </Head>
   );
