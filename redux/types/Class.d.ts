3bea53b80d955eb1194d199bbe86079aa0fbe92b
Sam Poate <sampoate@outlook.com>
2022-11-11 23:37:03 +0000

    Move types

--- redux/types/Class.d.ts
+++ redux/types/Class.d.ts
@@ -20,3 +20,19 @@ interface Item {
     }[];
   };
 }
+
+interface IClassWorkout {
+  name: string;
+  description: string;
+  order: number;
+  isNew?: boolean;
+  isDeleted?: boolean;
+}
+
+interface IClass {
+  name: string;
+  published: boolean;
+  created_at: FieldValue;
+  updated_at?: FieldValue;
+  isNew?: boolean;
+}
